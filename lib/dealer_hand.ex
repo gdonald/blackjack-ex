bde3c7660a0e69df0d50ab3074d93823905d3277
Greg Donald <gdonald@gmail.com>
2019-06-23 20:02:06 -0500

    Dealer hand value

--- lib/dealer_hand.ex
+++ lib/dealer_hand.ex
@@ -8,7 +8,7 @@ defmodule Blackjack.DealerHand do
       Enum.with_index(dealer_hand.hand.cards)
       |> Enum.map(
            fn ({card, index}) ->
-             DealerHand.down_card_val(dealer_hand, index, card)
+             DealerHand.card_val(dealer_hand, index, card)
            end
          )
       |> Hand.final_count(count_method)
@@ -18,8 +18,17 @@ defmodule Blackjack.DealerHand do
        else: total
   end
 
-  def down_card_val(dealer_hand, index, card) do
+  def card_val(dealer_hand, index, card) do
     if index == 0 && dealer_hand.hide_down_card,
        do: 0, else: Card.val(card)
+  end
+
+  def is_busted?(dealer_hand) do
+    DealerHand.get_value(dealer_hand, :soft) > 21
+  end
+
+  def up_card_is_ace?(dealer_hand) do
+    [first | _rest] = dealer_hand.hand.cards
+    Card.is_ace?(first)
   end
 end
--- spec/dealer_hand_spec.exs
+++ spec/dealer_hand_spec.exs
@@ -2,9 +2,10 @@ defmodule DealerHandSpec do
   use ESpec
   alias Blackjack.{DealerHand, Card, Hand}
 
+  let :ace, do: %Card{value: 0}
+  let :ten, do: %Card{value: 9}
+
   describe "DealerHand.get_value/2" do
-    let :ace, do: %Card{value: 0}
-    let :ten, do: %Card{value: 9}
     let :hand, do: %Hand{cards: [ace(), ten()]}
 
     context "down card is hidden" do
@@ -48,6 +49,84 @@ defmodule DealerHandSpec do
         expect DealerHand.get_value(dealer_hand(), :hard)
                |> to(eq 11)
       end
+    end
+  end
+
+  describe "DealerHand.card_val/3" do
+    context "down card is hidden" do
+      let :dealer_hand, do: %DealerHand{hide_down_card: true}
+
+      it "returns 0 for first card" do
+        expect DealerHand.card_val(dealer_hand(), 0, ten())
+               |> to(eq 0)
+      end
+
+      it "returns 10 for second card" do
+        expect DealerHand.card_val(dealer_hand(), 1, ten())
+               |> to(eq 10)
+      end
+    end
+
+    context "down card is visible" do
+      let :dealer_hand, do: %DealerHand{hide_down_card: false}
+
+      it "returns 10 for first card" do
+        expect DealerHand.card_val(dealer_hand(), 0, ten())
+               |> to(eq 10)
+      end
+
+      it "returns 10 for second card" do
+        expect DealerHand.card_val(dealer_hand(), 1, ten())
+               |> to(eq 10)
+      end
+    end
+  end
+
+  describe "DealerHand.is_busted?/1" do
+    let :eight, do: %Card{value: 7}
+
+    it "returns true" do
+      dealer_hand = %DealerHand{
+        hide_down_card: false,
+        hand: %Hand{
+          cards: [eight(), eight(), eight()]
+        }
+      }
+      expect DealerHand.is_busted?(dealer_hand)
+             |> to(be_true())
+    end
+
+    it "returns false" do
+      dealer_hand = %DealerHand{
+        hide_down_card: false,
+        hand: %Hand{
+          cards: [eight(), eight()]
+        }
+      }
+      expect DealerHand.is_busted?(dealer_hand)
+             |> to(be_false())
+    end
+  end
+
+  describe "DealerHand.up_card_is_ace?/1" do
+    it "returns true" do
+      dealer_hand = %DealerHand{
+        hand: %Hand{
+          cards: [ace(), ten()]
+        }
+      }
+      expect DealerHand.up_card_is_ace?(dealer_hand)
+             |> to(be_true())
+    end
+
+    it "returns false" do
+      dealer_hand = %DealerHand{
+        hand: %Hand{
+          cards: [ten(), ace()]
+        }
+      }
+      expect DealerHand.up_card_is_ace?(dealer_hand)
+             |> to(be_false())
     end
   end
 end
